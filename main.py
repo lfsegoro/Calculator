613fb24300cb028681040bd8d1b42d549fb7a8b7
Your Name <you@example.com>
2023-07-24 14:29:37 +0700

    Fully class, use match case, clean code residue.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 class Operand:
     def __init__(self, raw_num):
-        self.nUmber = int(raw_num)
- #      return Operand(self.nUmber + other)
+        self.nUmber = float(raw_num)
     def method_1(self):
         return (self.nUmber)
 
@@ -19,14 +18,15 @@ class Operation:
         if Operand(num2).method_1() != 0:
             self.division = Operand(num1).method_1() / Operand(num2).method_1()
     def manipulation(self):
-       if (Operator(symbol).method_2()) in ["+","plus","add"]:
-           return self.addition
-       if (Operator(symbol).method_2()) in ["-","minus","substract"]:
-           return self.substraction
-       if (Operator(symbol).method_2()) in ["*","x","times"]:
-           return self.multiplication
-       if (Operator(symbol).method_2()) in ["/",":","divide"]:
-           return self.division
+        match Operator(symbol).method_2():
+            case "+"| "plus"| "add":
+                return self.addition
+            case "-"|"minus"|"substract":
+                return self.substraction
+            case "*"|"x"|"times":
+                return self.multiplication
+            case "/"|":"|"divide":
+                return self.division
 
 class Result:
    def __init__(self):
@@ -34,44 +34,18 @@ class Result:
    def realshow(self):
        print (self.show)
 
+class exception:
+    pass
+
 # input operand like 8, eight, "one" etc.
 num1 = input(f'enter a number: ') # be input by user
-#operand1 = Operand(num1)
-#print(type(num1))
-#print(Operand(num1))
-#print(type(Operand(num1)))
-#print(Operand.method_1(num1))
-#print(type(Operand.method_1(num1)))
-#print(Operand(num1).method_1())
-#print(type(Operand(num1).method_1()))
-#print(type(operand1))
 
 # input operand like 8, eight, "one" etc.
 num2 = input("enter one more number: ") # be input by user
-#operand2 = Operand(num2)
-#print (operand2.method_1())
 
 # operator like * x : / + -, divide, add , multiply etc.
-symbol = input('enter operator, I accept all these ["+","plus","add"]["-","minus","substract"]["*","x","times"]["/",":","divide"]: ') #be input by user
-#print (operator.method_2())
-"""
-print(Operator(symbol))
-print(Operator(symbol).method_2())
-print(type(Operator(symbol)))
-print(type(Operator(symbol).method_2()))
-"""
+symbol = input('enter operator, I accept all these ["+","plus","add"]\n["-","minus","substract"]\n["*","x","times"]\n["/",":","divide"]: ') #be input by user
 
-#print (Operation(num1,num2).manipulation())
-#or
+# Result :
 Output = Result().realshow()
 
-# Result of the operation
-#result = Operation()
-#if (operator.method_2() in ["*","x","time"]):
-#    print (result.multiplication(operand1.method_1(),operand2.method_1()))
-#if (operator.method_2() in ["/",":","divide"]):
-#    print (result.division(operand1.method_1(),operand2.method_1()))
-#if (operator.method_2() in ["+","plus","add"]):
-#    print (result.addition(operand1.method_1(),operand2.method_1()))
-#if (operator.method_2() in ["-","minus","substract"]):
-#    print (result.substraction(operand1.method_1(),operand2.method_1()))
